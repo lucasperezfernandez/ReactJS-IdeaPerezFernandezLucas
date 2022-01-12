a4485e966c07604ff4166c772bb9e54884619034
Lucas Perez Fernandez <lucasperezfernandez@hotmail.com>
2022-01-12 20:30:56 -0300

    se agrega NavBar basica para entrega

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+
+function NavBar() {
+    return <>
+        {/* Creamos barra para el navegador */}
+        <div className="NavBar">
+            <h2 className="NavBar-Heading">GeekSpot</h2>
+            <h3>Seccion 1</h3>
+            <h3>Seccion 2</h3>
+            <h3>Seccion 3</h3>
+        </div>
+    </>
+};
+
+export default NavBar;
