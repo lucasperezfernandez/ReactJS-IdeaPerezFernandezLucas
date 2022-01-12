6573820d957e9906587bd58d5ae32224cb7feb8a
Lucas Perez Fernandez <lucasperezfernandez@hotmail.com>
2022-01-12 20:30:06 -0300

    Se agrega NavBar basica para entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/NavBar/NavBar.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div>
+      <NavBar />
+      <h3>Hola Mundo</h3>
     </div>
   );
 }
