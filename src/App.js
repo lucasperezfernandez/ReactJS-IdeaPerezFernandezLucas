ded762cb3facf96cc712e4495ceda8c5aaa676c5
Lucas Perez Fernandez <lucasperezfernandez@hotmail.com>
2022-01-30 21:46:31 -0300

    cambios en los errores entrega 7

--- src/App.js
+++ src/App.js
@@ -2,45 +2,24 @@ import './App.css';
 // import './components/NavBar/NavBar.css';
 // import NavBar from './components/NavBar/NavBar';
 // import ItemListContainer from './components/IndexBody/ItemListContainer';
-// // import MyClassComponent from './components/MyClassComponent';
-// // import MyFunctionalComponent from './components/MyFunctionalComponent';
-// import ItemCount from './components/ItemCount';
+// import MyClassComponent from './components/MyClassComponent';
+// import MyFunctionalComponent from './components/MyFunctionalComponent';
+import ItemCount from './components/ItemCount';
 import { useEffect, useState } from 'react';
+import NavBar from './components/NavBar/NavBar';
+import GetProducts from './components/items/Item';
+
 // import { getProductos } from './components/items/Item';
 
 function App() {
-  const [poke, setPoke] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-
-  useEffect(() => {
-    const URL = 'http://localhost:3500/items'
-
-    setIsLoading(true)
-    fetch(URL)
-      .then((response) => response.json())
-      .then((json) => setPoke(json))
-      .catch((err) => setError (err))
-      .finally(() => setIsLoading(false));
-  }, []);
-
-  if(isLoading){
-    return <p>Carrgando los poke...</p>
-  }else if (error){
-    return <p>Ha habido un error {error.message}</p>
-  }else {
-    return(
-      <div>
-        <h1>Hola</h1>
-        <ul>
-          {poke.map((items) => {
-            return <li key={items.id}>{items.title}</li>;
-          })}
-        </ul>
-      </div>
-    )
-  }
+return (
+  <div>
+    <NavBar></NavBar>
+    <h2>Titulo</h2>
+    <GetProducts></GetProducts>
+    <ItemCount name="Boton Prueba" stock= {5}/>
+  </div>
+)
 
 }
 export default App;
@@ -59,17 +38,5 @@ export default App;
     //   {/* <MyClassComponent/>
     //   <MyFunctionalComponent/> */}
     //   <ItemCount name="Boton Prueba" stock= {5}/>
-    //   <ul>
-    //     {products.map((product) => (
-    //         <li key={ product.id }> {product.name} precio: {product.price} </li>
-    //     ))}
-    //   </ul>
-    // </div>
 
-    // ASYNC DENTRO DEL USE EFFECT EN FUNCION
-        // const getPoke = async() => {
-    //   const response = await fetch(URL);
-    //   const json = response.json()
-    //   console.log(json)
-    // };
-    // getPoke(); 
+
--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,13 +1,40 @@
-export const products = [
-    {id: 0, name: "Macbook Pro", price: 1200},
-    {id: 1, name: "PlayStation", price: 600},
-    {id: 2, name: "Nintendo Switch", price: 450},
-    {id: 3, name: "Parlante", price: 150},
-];
+import { useEffect, useState } from 'react';
 
 
-export function getProductos() {
-    return new Promise(( resolve, reject) => {
-        setTimeout(() => resolve(products), 2000);
-    });
+
+function GetProducts() {
+    const [poke, setPoke] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        const URL = 'http://localhost:3500/items'
+
+        setIsLoading(true)
+        fetch(URL)
+            .then((response) => response.json())
+            .then((json) => setPoke(json))
+            .catch((err) => setError(err))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    if (isLoading) {
+        return <p>Carrgando los poke...</p>
+    } else if (error) {
+        return <p>Ha habido un error {error.message}</p>
+    } else {
+        return (
+            <div>
+                <h1>Productos</h1>
+                <ul>
+                    {poke.map((items) => {
+                        return <li key={items.id}>{items.title}, <p>precio: {items.price}.</p> <p>Detalle: {items.detail}</p></li>;
+                        
+                    })}
+                </ul>
+            </div>
+        )
+    }
 }
+export default GetProducts;
