dc969ff2d7968d6afd96dc59f382ce6026549f42
Lucas Perez Fernandez <lucasperezfernandez@hotmail.com>
2022-01-31 23:14:01 -0300

    cambios en el estilo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,6 +10,8 @@ const HomePage = () => {
     return (
         <div>
             <h1>Home Page</h1>
+            <h3>Esto es geek spot, el lugar en donde siempre encontras lo que necesitas</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque doloribus eius laboriosam possimus alias fuga ipsum tenetur velit nesciunt dolorum, officiis cupiditate quisquam natus placeat dignissimos repellendus, neque, porro error!</p>
             <button onClick={goToProducts}>Ir a la pagina de productos</button>
         </div>
     );
