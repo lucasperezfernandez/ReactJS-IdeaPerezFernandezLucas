1cecdb8a6b529bc47dafe86c50570a55487b072b
Lucas Perez Fernandez <lucasperezfernandez@hotmail.com>
2022-02-03 01:44:40 -0300

    cambios en el contador para la entrega

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -1,29 +1,15 @@
 import React from "react";
 
-export default function ItemCount({stock}){
-    const [counter, setCounter] = React.useState(1)
-
-    const less = () => {
-        setCounter(counter - 1);
-        if( counter <= 0 ){
-            alert("Cantidad minima");
-            setCounter(0)
-        }
-    }
-    const more = () => {
-        setCounter(counter + 1);
-        if( counter >= stock){
-            alert("No puede superar la cantidad de stock maxima")
-            setCounter(stock);
-        }
-    }
+const ItemCount = ({less , more, agregarCarro}) =>{
     return (
         <>
-        <p>Cantidad deseada: {counter}</p>
         <button onClick={less}>-</button>
         <button onClick={more}>+</button>
-        <p></p>
-        </>
+        <div></div>
+        <button onClick={agregarCarro}>agregar al Carro</button>
         
+        </>
     );
 }
+
+export default ItemCount
--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -2,11 +2,39 @@ import { useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import ItemCount from "../components/ItemCount";
 import "./styles.scss";
+import React from "react";
 
 const ProductDetailPage = () => {
     const {productId} = useParams();
     const [product, setProduct] = useState();
     const [isLoading, setIsLoading] = useState(false);
+    const [counter, setCounter] = React.useState(1)
+
+    const less = () => {
+        setCounter((prevState) => prevState - 1);
+        if( counter <= 0 ){
+            alert("Cantidad minima");
+            setCounter(0)
+        }
+    }
+    const more = (evt) => {
+        evt.stopPropagation();
+        setCounter((prevState) => prevState + 1)
+        if( counter >= product.stock){
+            alert("No puede superar la cantidad de stock maxima")
+            setCounter(product.stock);
+        }
+    }
+    const agregarCarro = () =>{
+        let total = product.price * counter;
+        console.log("Se agrega el siguiente monto a pagar:" + total + "$");
+        setCounter(0);
+    }
+    function totalParcial(precio, cantidad) {
+        let parcial = precio * cantidad;
+        return parcial;
+    }
+
     // console.log (5 === +productId);
     useEffect(()=>{
         const URL = `http://localhost:3500/items/${productId}`;
@@ -24,8 +52,9 @@ const ProductDetailPage = () => {
                 <p>{product.detail}</p>
                 <p>Precio: {product.price} $</p>
                 <p>Stock: {product.stock}</p>
-                <ItemCount stock={product.stock}/>
-                <button >Total</button>
+                <p>cantidad deseada:{counter}</p>
+                <ItemCount less={less} more={more} agregarCarro={agregarCarro}  />
+                <p>total: {totalParcial(product.price, counter)}</p>
             </div>
         );
 };
